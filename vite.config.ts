644187974ca616d03593cae1121fadbaf9088dd3
vuhe <zhuhe202@qq.com>
2026-01-26 17:56:59 +0800

    优化 vite 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // biome-ignore lint/style/noDefaultExport: https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
     tailwindcss(),
     react({
       babel: {
