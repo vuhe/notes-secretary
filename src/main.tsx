49a0c6e679cb701d187b6c41f777dacf90e9d32c
vuhe <zhuhe202@qq.com>
2026-01-26 16:05:36 +0800

    初始化项目

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App.tsx";
+
+// biome-ignore lint/style/noNonNullAssertion: must be non-null
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
